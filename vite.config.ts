d9116a835ab9d7bd007e66527026a01bb3490b3d
Gexary <gexary.gex@gmail.com>
2025-08-21 01:42:01 +0100

    fixx

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { BASE_URL } from "./src/config/constants";
 
 // https://vite.dev/config/
 export default defineConfig({
